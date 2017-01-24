318ff616dec04050fbe45f59a0ba58149da843af
Jacob Oyen <oyenj@central.edu>
2017-01-24 07:27:58 -0600

    Simplifies plugin for future use.

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 /*
 Plugin Name: C2AP for WordPress
 Plugin URI: https://github.com/jacoboyen/expanded-wp-profiles
-Description: Adds expanded features to WordPress sites that integrate with www.central.edu
-Version: 0.01
+Description: Core functionality for Central College WordPress installs.
+Version: 2.0.0
 Author: Jacob Oyen (@jacoboyen)
 */
 
 // -----------------------------------------------------------------------
 // Expanded user profiles
 // -----------------------------------------------------------------------
-function show_expanded_profile_fields($user){?>
+function cui_show_expanded_profile_fields($user){?>
 	<h3>Social Media</h3>
     <table class="form-table">
     	<tbody>
@@ -20,7 +20,7 @@ function show_expanded_profile_fields($user){?>
                 </th>
                 <td>
                 	<input type="text" name="twitter" id="twitter" value="<?php echo esc_attr( get_the_author_meta( 'twitter', $user->ID ) ); ?>" />
-                    <span class="description">Enter the full twitter URL for your account: http://twitter.com/centralcollege/</span>
+                    <span class="description">Enter your twitter user: Example: @centralcollege</span>
                 </td>
 			</tr>
             <tr>
@@ -36,66 +36,29 @@ function show_expanded_profile_fields($user){?>
 	</table>
 <?php }
 
-add_action('show_user_profile', 'show_expanded_profile_fields');
-add_action('edit_user_profile', 'show_expanded_profile_fields');
+add_action('show_user_profile', 'cui_show_expanded_profile_fields');
+add_action('edit_user_profile', 'cui_show_expanded_profile_fields');
 
-function save_expanded_profile_fields($user_id){
+function cui_save_expanded_profile_fields($user_id){
 	if (!current_user_can('edit_user', $user_id)) return false;
-	
+
 		update_usermeta($user_id, 'twitter', $_POST['twitter']);
 		update_usermeta($user_id, 'facebook', $_POST['facebook']);
 }
-add_action( 'personal_options_update', 'save_expanded_profile_fields' );
-add_action( 'edit_user_profile_update', 'save_expanded_profile_fields' );
+add_action( 'personal_options_update', 'cui_save_expanded_profile_fields' );
+add_action( 'edit_user_profile_update', 'cui_save_expanded_profile_fields' );
 
-// -----------------------------------------------------------------------
-// Add WordPress administation screens
-// -----------------------------------------------------------------------
-function central_plugin_menu(){
-	/*add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );*/
-	add_menu_page('Central Config Home', 'C2AP for WP', 'edit_pages', 'central-config', 'central_plugin_menu_display', '//img.centralcollege.info/icons/animal-monkey.png', 99);
-	add_submenu_page('central-config', 'Text Area Restrictions', 'Text Area Restrictions', 'manage_options', 'tiny-mce', 'central_plugin_menu_display_tinyMCE' );
-	add_submenu_page('central-config', 'Directory Listing', 'Directory Listing', 'edit_pages', 'directory-listing', 'central_plugin_menu_display_directory' );
-	add_submenu_page('central-config', 'C2AP login', 'C2AP login', 'edit_pages', 'c2ap-login', 'central_plugin_menu_display_C2APlogin' );
-}
-// Central Config Page
-function central_plugin_menu_display(){
-	include 'dashboard.php';	
-}
-// Text Area Restrictions Page
-function central_plugin_menu_display_tinyMCE(){
-	include 'tiny-MCE-restrictions.php';
-}
-// Directory Listing Page
-function central_plugin_menu_display_directory(){
-	include 'directory.php';
-}
-// C2AP login
-function central_plugin_menu_display_C2APlogin(){
-	include 'c2ap.php';
-}
-add_action('admin_menu', 'central_plugin_menu');
-
-// -----------------------------------------------------------------------
-// Behind the scenes things that we want this plugin to do
-// -----------------------------------------------------------------------
-//Disable pingbacks
-add_filter('xmlrpc_methods', 'remove_xmlrpc_pingback');
-function remove_xmlrpc_pingback($methods){
-	unset($methods['pingback.ping']);
-	return $methods;
-}
 //Remove admin color schemes
-function remove_admin_color_scheme(){
+function cui_remove_admin_color_scheme(){
 	global $_wp_admin_css_colors;
 	$_wp_admin_css_colors = 0;
 }
-add_action('admin_head', 'remove_admin_color_scheme');
+add_action('admin_head', 'cui_remove_admin_color_scheme');
 
-//Set session timeout to 20 minutes
-function central_cookie_expiration($expiration){
-	return 1200;
+//Set session timeout to 60 minutes
+function cui_cookie_expiration($expiration){
+	return 3600;
 }
-add_filter('auth_cookie_expiration', 'central_cookie_expiration');
+add_filter('auth_cookie_expiration', 'cui_central_cookie_expiration');
 
 ?>
--- readme.md
+++ readme.md
@@ -3,15 +3,8 @@ C2AP for WordPress
 
 A collection of C2AP features for use with WordPress.
 
-Requirements
-------------------
-* WordPress Administrative Access
-
 Features
 ------------------
-* One click directory page creation.
-* Expanded profile fields
-* Tiny MCE restrictions (in progress)
-* Removes XML-RPC pingbacks and trackbacks
+* Expands profile field information.
 * Removes admin color schemes
-* Changes timeout length
+* Changes timeout length to one hour
